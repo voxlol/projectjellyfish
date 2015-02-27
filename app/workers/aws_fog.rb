9fe6dd58c8e2cdfcfb8857de82fc8d49fd19ca85
Jillian Tullo <tullo_jillian@bah.com>
2015-02-27 12:56:36 -0500

    - Code refactoring - Error checking

--- app/workers/aws_fog.rb
+++ app/workers/aws_fog.rb
@@ -1,12 +1,10 @@
 class AwsFog
   def initialize(order_item_id)
-    Delayed::Worker.logger.debug 'Initializing Fog'
     @order_item_id = order_item_id
-    Fog.mock! # if ENV['MOCK_MODE'] == 'true'
+    Fog.mock! if ENV['MOCK_MODE'] == 'true'
   end
 
   def provision
-    Delayed::Worker.logger.debug "Your password: #{order_item}"
     product_type = order_item.product.product_type.name.capitalize.downcase
     method_call = method("provision_#{product_type}")
     method_call.call
@@ -32,7 +30,7 @@ class AwsFog
     answers = order_item.product.answers
     order_item.product.product_type.questions.each do |question|
       answer = answers.select { |row| row.product_type_question_id == question.id }.first
-      if questions.manageiq_key == 'db_instance_class'
+      if question.manageiq_key == 'db_instance_class'
         details['DBInstanceClass'] = answer.nil? ? question.default : answer.answer
       else
         details[question.manageiq_key.camelize] = answer.nil? ? question.default : answer.answer
@@ -46,7 +44,8 @@ class AwsFog
   end
 
   def provision_infrastructure
-    aws_connection = Fog::Compute.new(
+    Delayed::Worker.logger.debug 'Provisioning Infrastructure'
+    @aws_connection = Fog::Compute.new(
       provider: 'AWS',
       aws_access_key_id: aws_settings[:access_key],
       aws_secret_access_key: aws_settings[:secret_key]
@@ -55,16 +54,23 @@ class AwsFog
     # TODO: Must get an image_id from product types
     details['image_id'] = 'ami-acca47c4'
     begin
-      server = aws_connection.servers.create(details)
-      server.wait_for { ready? }
+      create_infrastructure(details)
+    rescue Excon::Errors::BadRequest
+      Delayed::Worker.logger.debug "Bad request: #{e.message}"
+      order_item.provision_status = 'critical'
+      order_item.status_msg = 'Bad request. Check authorization credentials.'
+      order_item.save
     rescue StandardError => e
-      Delayed::Worker.logger.debug "Caught standard error #{e.message}"
+      Delayed::Worker.logger.debug "Caught standard error: #{e.message}"
       order_item.provision_status = 'critical'
       order_item.status_msg = e.message
       order_item.save
-      exit
     end
-    # Store the created server information
+  end
+
+  def create_infrastructure(details)
+    server = @aws_connection.servers.create(details)
+    server.wait_for { ready? }
     order_item.public_ip = server.public_ip_address
     order_item.instance_id = server.id
     order_item.private_ip = server.private_ip_address
@@ -82,21 +88,23 @@ class AwsFog
     instance_name = "id-#{order_item.uuid[0..9]}"
     begin
       storage = aws_connection.directories.create(
-        key: order_item.uuid[0..9], # key is the name of the bucket
+        key: instance_name,
         public: true
       )
-    rescue StandardError => e
+      order_item.url = storage.public_url
+      order_item.instance_name = instance_name
+      order_item.provision_status = 'ok'
+      order_item.save
+    rescue Excon::Errors::BadRequest
       order_item.provision_status = 'critical'
-      Delayed::Worker.logger.debug "Error message: #{e.message}"
-      Delayed::Worker.logger.debug "Cause: #{e.cause}"
+      order_item.status_msg = 'Bad request. Check authorization credentials.'
+      order_item.save
+    rescue StandardError => e
+      Delayed::Worker.logger.debug "Caught standard error: #{e.message}"
+      order_item.provision_status = 'critical'
       order_item.status_msg = e.message
       order_item.save
-      exit
     end
-    order_item.url = storage.public_url
-    order_item.instance_name = instance_name
-    order_item.provision_status = 'ok'
-    order_item.save
   end
 
   def provision_databases
@@ -105,16 +113,29 @@ class AwsFog
       aws_secret_access_key: aws_settings[:secret_key]
     )
     sec_pw = SecureRandom.hex[0..9]
-    options = order_item_details_camelize
+    details = order_item_details_camelize
     # TODO: Figure out solution for camelcase / snake case issues
-    options['MasterUserPassword'] = sec_pw
-    options['MasterUsername'] = 'admin'
-    Delayed::Worker.logger.debug "Updated details: #{options}"
+    details['MasterUserPassword'] = sec_pw
+    details['MasterUsername'] = 'admin'
     db_instance_id = "id-#{@order_item.uuid[0..9]}"
-    db = @aws_connection.create_db_instance(db_instance_id, options)
+    begin
+      create_database(details, db_instance_id)
+    rescue Excon::Errors::BadRequest
+      order_item.provision_status = 'critical'
+      order_item.status_msg = 'Bad request. Check authorization credentials.'
+      order_item.save
+    rescue StandardError => e
+      order_item.provision_status = 'critical'
+      order_item.status_msg = e.message
+      order_item.save
+    end
+  end
+
+  def create_database(details, db_instance_id)
+    db = @aws_connection.create_db_instance(db_instance_id, details)
     order_item.provision_status = 'ok'
     order_item.username = 'admin'
-    order_item.password = BCrypt::Password.create(sec_pw)
+    order_item.password = BCrypt::Password.create(@sec_pw)
     order_item.instance_name = db_instance_id
     order_item.port = db.local_port
     order_item.url = db.local_address
