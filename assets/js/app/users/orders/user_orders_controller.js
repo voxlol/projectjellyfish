edf0ae0152526a2268d0c3167d75a1b31d6db517
G Klausner <gnklausner@gmail.com>
2015-03-13 14:59:47 -0400

    Updated status column in order history table

--- assets/js/app/users/orders/user_orders_controller.js
+++ assets/js/app/users/orders/user_orders_controller.js
@@ -4,20 +4,39 @@ var _ = require('lodash');
 
 /**@ngInject*/
 function UserOrderController($scope, orders) {
-    var provision_status_map = { ok: 0, warning: 1, critical: 2, unknown: 3, pending: 4, retired: 5, blank: 6 };
+    var provision_status_map = { ok: 0, warning: 1, critical: 2, unknown: 3, pending: 4, retired: 5 };
     _.each(orders, function(order)
     {
-        order.provision_status = "blank";
-        _.each(order.order_items, function(order_item) //can't break out of, so may want to use a for loop and break at lowest status
+        order.provision_status = "ok";
+        var retiredCount = 0;
+        _.each(order.order_items, function(order_item)
         {
-                if ((typeof order_item.provision_status !== "undefined") && (provision_status_map[order.provision_status] > provision_status_map[order_item.provision_status]))
+            if (typeof order_item.provision_status === "undefined")
+            {
+                order_item.provision_status = "unknown";
+            }
+            if (provision_status_map[order.provision_status] < provision_status_map[order_item.provision_status])
+            {
+                if (order_item.provision_status === "retired")
+                {
+                    retiredCount++;
+                }
+                else
                 {
                     order.provision_status = order_item.provision_status;
                 }
+            }
         });
-        if (order.provision_status === "blank")
+        if (retiredCount == order.order_items.length)
         {
-            order.provision_status = "";
+            if (retiredCount > 0)
+            {
+                order.provision_status = "retired";
+            }
+            else
+            {
+                order.provision_status = "";
+            }
         }
     });
     this.orders = orders;
