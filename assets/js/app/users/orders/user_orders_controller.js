f31f250ec87c95a3df5d7bfd4a8fb29a17753f68
G Klausner <gnklausner@gmail.com>
2015-03-16 18:15:26 -0400

    Rebuilt based upon Stack's suggestions

--- assets/js/app/users/orders/user_orders_controller.js
+++ assets/js/app/users/orders/user_orders_controller.js
@@ -7,45 +7,36 @@ function UserOrderController($scope, orders) {
   this.orders = this.getOrderStatus(orders);
 }
 
+function _reduceItemStatus(status, order_item) {
+  var itemStatus = null;
+  var provision_status_map = { ok: 0, pending: 1, unknown: 2, warning: 3, critical: 4, retired: 5 };
+  // undefined status and 'unknown' statuses are set to 'warning' for order status
+  if ((typeof order_item.provision_status === "undefined") || (order_item.provision_status === "unknown")) {
+    itemStatus = "warning";
+  } else {
+    itemStatus = order_item.provision_status;
+  }
+  // Compare levels of the statuses - the higher status should represent the status of the order (except retired)
+  if ((provision_status_map[itemStatus] > provision_status_map[status]) && (itemStatus !== "retired")) {
+    return itemStatus;
+  } else {
+    // Return existing status
+    return status;
+  }
+}
+
 UserOrderController.prototype.getOrders = function() {
   return this.orders;
 };
 
 UserOrderController.prototype.getOrderStatus = function(orders) {
-  // this enumeration of statuses is not consistent with the API's enumeration.
-  var provision_status_map = { ok: 0, pending: 1, unknown: 2, warning: 3, critical: 4, retired: 5 };
   _.each(orders, function(order)
   {
-    order.provision_status = "ok";
-    var retiredCount = 0;
-    _.each(order.order_items, function(order_item)
-    {
-      if (typeof order_item.provision_status === "undefined") {
-        order_item.provision_status = "unknown";
-      }
-      // if an order_item's status is of higher concern than the order's current status
-      if (provision_status_map[order.provision_status] < provision_status_map[order_item.provision_status]) {
-        if (order_item.provision_status === "retired") {
-          retiredCount++;
-        }
-        // unknown is considered a warning for the order
-        else if (order_item.provision_status === "unknown") {
-          order.provision_status = "warning";
-        }
-        else {
-          order.provision_status = order_item.provision_status;
-        }
-      }
-    });
-    // not retired unless all order_items are retired
-    if (retiredCount === order.order_items.length)
-    {
-      if (retiredCount > 0) {
-        order.provision_status = "retired";
-      }
-      else {
-        order.provision_status = "";
-      }
+    if (_.every(order.order_items, function(order_item){return order_item.provision_status === "retired";})) {
+      order.provision_status = "retired";
+    }
+    else {
+      order.provision_status = _.reduce(order.order_items, _reduceItemStatus, "ok");
     }
   });
   return orders;
