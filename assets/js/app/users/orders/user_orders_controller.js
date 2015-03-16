897acefe076f7dcf4290cdd2b6c1387cf550f8b9
G Klausner <gnklausner@gmail.com>
2015-03-16 12:50:59 -0400

    Order history table, status column: unknown == warning

--- assets/js/app/users/orders/user_orders_controller.js
+++ assets/js/app/users/orders/user_orders_controller.js
@@ -4,53 +4,51 @@ var _ = require('lodash');
 
 /**@ngInject*/
 function UserOrderController($scope, orders) {
-    var provision_status_map = { ok: 0, warning: 1, critical: 2, unknown: 3, pending: 4, retired: 5 };
-    _.each(orders, function(order)
+  var provision_status_map = { ok: 0, warning: 3, critical: 4, unknown: 2, pending: 1, retired: 5 };
+  _.each(orders, function(order)
+  {
+    order.provision_status = "ok";
+    var retiredCount = 0;
+    _.each(order.order_items, function(order_item)
     {
-        order.provision_status = "ok";
-        var retiredCount = 0;
-        _.each(order.order_items, function(order_item)
-        {
-            if (typeof order_item.provision_status === "undefined")
-            {
-                order_item.provision_status = "unknown";
-            }
-            if (provision_status_map[order.provision_status] < provision_status_map[order_item.provision_status])
-            {
-                if (order_item.provision_status === "retired")
-                {
-                    retiredCount++;
-                }
-                else
-                {
-                    order.provision_status = order_item.provision_status;
-                }
-            }
-        });
-        if (retiredCount == order.order_items.length)
-        {
-            if (retiredCount > 0)
-            {
-                order.provision_status = "retired";
-            }
-            else
-            {
-                order.provision_status = "";
-            }
+      if (typeof order_item.provision_status === "undefined") {
+        order_item.provision_status = "unknown";
+      }
+      if (provision_status_map[order.provision_status] < provision_status_map[order_item.provision_status]) {
+        if (order_item.provision_status === "retired") {
+          retiredCount++;
         }
+        // unknown is considered a warning for the order
+        else if (order_item.provision_status === "unknown") {
+          order.provision_status = "warning";
+        }
+        else {
+          order.provision_status = order_item.provision_status;
+        }
+      }
     });
-    this.orders = orders;
+    if (retiredCount === order.order_items.length)
+    {
+      if (retiredCount > 0) {
+        order.provision_status = "retired";
+      }
+      else {
+        order.provision_status = "";
+      }
+    }
+  });
+  this.orders = orders;
 }
 
 UserOrderController.prototype.getOrders = function() {
-    return this.orders;
+  return this.orders;
 };
 
 UserOrderController.resolve = {
-    /**@ngInject*/
-    orders: function(UserOrders, currentUser) {
-        return UserOrders.query({staff_id: currentUser.id}).$promise;
-    }
+  /**@ngInject*/
+  orders: function(UserOrders, currentUser) {
+    return UserOrders.query({staff_id: currentUser.id}).$promise;
+  }
 };
 
 module.exports = UserOrderController;
