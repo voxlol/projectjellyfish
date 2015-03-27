1717143a3400d9c71e6ca8aa7a6c3aa13d156ccb
Christopher Kacerguis <kacerguis_christopher@bah.com>
2015-03-27 11:13:01 -0500

    removed node_modules artifact, updated app.json for more changed to heroku, updated headers.

--- app/serializers/content_page_serializer.rb
+++ app/serializers/content_page_serializer.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: content_pages
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  deleted_at :datetime
+#  staff_id   :integer
+#  slug       :string(255)      not null
+#  title      :string(255)      not null
+#  body       :text
+#
+# Indexes
+#
+#  index_content_pages_on_slug      (slug) UNIQUE
+#  index_content_pages_on_staff_id  (staff_id)
+#
+
 class ContentPageSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at, :deleted_at, :staff_id, :slug, :title, :body
 end
