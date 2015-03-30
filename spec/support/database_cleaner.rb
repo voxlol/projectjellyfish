2f4c8ec853b4cc59a867debd3a366e5530647b74
Mason Fischer <mason@thoughtbot.com>
2015-03-30 13:43:34 -0500

    Clean integration tests with truncation

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,10 +1,14 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:deletion)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :integration) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each, :js => true) do
