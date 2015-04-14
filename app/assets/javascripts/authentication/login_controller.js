bb8a57f5e971173016ae6cb1f953fa94ea6e1872
AllenBW <wight_allen@bah.com>
2015-04-14 10:40:51 -0400

    addressing houndci comments

--- app/assets/javascripts/authentication/login_controller.js
+++ app/assets/javascripts/authentication/login_controller.js
@@ -5,65 +5,64 @@ var isFailedLogin = false;
 
 /**@ngInject*/
 function LoginController($scope, $state, AuthService, currentUser, ssoUrl) {
-    this.$scope = $scope;
-    this.$state = $state;
-    this.AuthService = AuthService;
-    this.ssoUrl = ssoUrl;
+  this.$scope = $scope;
+  this.$state = $state;
+  this.AuthService = AuthService;
+  this.ssoUrl = ssoUrl;
 
-    // If the user is already logged in, take them to the default route.
-    if (currentUser || AuthService.isAuthenticated()) {
-        $state.transitionTo('base.authed.dashboard');
-    }
+  // If the user is already logged in, take them to the default route.
+  if (currentUser || AuthService.isAuthenticated()) {
+    $state.transitionTo('base.authed.dashboard');
+  }
 }
 
 LoginController.prototype = {
 
-    login: function(sso) {
-        if(sso != undefined){
-            window.location=sso;
-        } else {
-            // Reset the failed login flag.
-            isFailedLogin = false;
+  login: function (sso) {
+    if (sso !== undefined) {
+      window.location = sso;
+    } else {
+      // Reset the failed login flag.
+      isFailedLogin = false;
 
-            var credentials = {
-                staff: {
-                    email: this.$scope.email,
-                    password: this.$scope.password
-                }
-            };
-
-            // @todo Add optional to redirect back to where they were instead of always going to dashboard.
-            this.AuthService.login(credentials)
-                .success(_.bind(function () {
-                    this.$state.transitionTo('base.authed.dashboard');
-                }, this))
-                .error(_.bind(function () {
-                    isFailedLogin = true;
-                }, this));
+      var credentials = {
+        staff: {
+          email: this.$scope.email,
+          password: this.$scope.password
         }
-    },
+      };
 
-    hasFailedLogin: function() {
-        return isFailedLogin;
+      // @todo Add optional redirect  to origin rather than dashboard.
+      this.AuthService.login(credentials).success(_.bind(function () {
+          this.$state.transitionTo('base.authed.dashboard');
+        }, this))
+        .error(_.bind(function () {
+          isFailedLogin = true;
+        }, this));
     }
+  },
+
+  hasFailedLogin: function () {
+    return isFailedLogin;
+  }
 };
 
 LoginController.resolve = {
-    /**@ngInject*/
-    ssoUrl: function(AuthService) {
-        return AuthService.ssoInit();
-    },
-    /**@ngInject*/
-    currentUser: function($q, UsersResource) {
-        var deferred = $q.defer();
+  /**@ngInject*/
+  ssoUrl: function (AuthService) {
+    return AuthService.ssoInit();
+  },
+  /**@ngInject*/
+  currentUser: function ($q, UsersResource) {
+    var deferred = $q.defer();
 
-        UsersResource.getCurrentMember(function() {
-            deferred.resolve(true);
-        }, function() {
-            deferred.resolve(false);
-        });
-        return deferred.promise;
-    }
+    UsersResource.getCurrentMember(function () {
+      deferred.resolve(true);
+    }, function () {
+      deferred.resolve(false);
+    });
+    return deferred.promise;
+  }
 };
 
 window.LoginController = LoginController;
