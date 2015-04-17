cc88cc1168e38c0dd62c0101140a46f480caa7d0
AllenBW <wight_allen@bah.com>
2015-04-17 12:36:11 -0400

    resolve travis.ci build issues

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -9,6 +9,6 @@ feature 'Staff signs in' do
     fill_in 'password', with: staff.password
     click_on 'Login'
 
-    expect(page).to have_content("#{staff.first_name} #{staff.last_name}")
+    expect(page).to have_css("#{staff.first_name} #{staff.last_name}")
   end
 end
