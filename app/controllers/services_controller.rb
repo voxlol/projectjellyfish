d65a85d16c335ebbfe955f6a82013afb7c304a39
mafernando <fernando_michael@bah.com>
2015-05-01 14:09:19 -0500

    adds logic to order_profiles route

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -80,8 +80,14 @@ class ServicesController < ApplicationController
   api :GET, '/services/order_profiles', 'Lists project orders with start date and order item count'
   def order_profiles
     authorize Service.new
-    project_orders = { key: 'Project Jellyfish', values: [[1_083_297_600_000, 12], [1_083_224_600_000, 4]] }
-    render json: project_orders
+    load_project_orders_via_sql
+    project_orders = {}
+    @project_orders.each do |p|
+      project_key = p.project_name
+      project_orders[project_key] = { key: project_key, values: [] } unless project_orders.key? project_key
+      project_orders[project_key][:values] << [p.order_created_at, p.order_item_count]
+    end
+    render json: project_orders.values
   end
 
   private
@@ -90,24 +96,41 @@ class ServicesController < ApplicationController
     @services = policy_scope(Service)
   end
 
+  def load_project_orders_via_sql
+    # GET PROJECTS SCOPED TO USER
+    load_projects
+    # QUERY OUT DESIRED ATTRIBUTES - TODO: FIGURE OUT A BETTER WAY TO DO THIS
+    @project_orders = @projects.joins(:services).joins('LEFT JOIN orders ON order_items.order_id = orders.id').select('
+      projects.name as project_name,
+      orders.id as id,
+      MIN(orders.created_at) as order_created_at,
+      COUNT(order_items.id) as order_item_count
+    ').group('projects.name, orders.id')
+  end
+
   def load_services_via_sql
-    # GET PROJECTS SCOPED TO USER AND GET TAGGED ORDER ITEMS (SERVICES)
-    projects = policy_scope(Project.all)
+    # GET PROJECTS SCOPED TO USER
+    load_projects
 
     # CREATE PROJECT ID LIST
-    project_ids = []
-    projects.each do |project|
-      project_ids << project.id
+    @project_ids = []
+    @projects.each do |project|
+      @project_ids << project.id
     end
 
     # QUERY OUT DESIRED ATTRIBUTES - TODO: FIGURE OUT A BETTER WAY TO DO THIS
     @services = OrderItem.select(
       'order_items.*,
-  projects.name as project_name,
-  projects.description as project_description,
-  products.name as service_name,
-  products.description as service_description'
-    ).from('order_items').where(project_id: project_ids).joins(:project, :product)
+    projects.name as project_name,
+    projects.description as project_description,
+    products.name as service_name,
+    products.description as service_description'
+    ).from('order_items').where(project_id: @project_ids).joins(:project, :product)
+  end
+
+  def load_projects
+    # GET PROJECTS SCOPED TO USER
+    @projects = policy_scope(Project.all)
   end
 
   def load_tagged_services
