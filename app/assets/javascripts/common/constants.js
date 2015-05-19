c130efb4aa21aa274077ff6fcb027a8933891108
Caleb Thompson <caleb@calebthompson.io>
2015-05-19 10:42:42 -0500

    Move api-base-path to meta tag

--- app/assets/javascripts/common/constants.js
+++ app/assets/javascripts/common/constants.js
@@ -1,23 +1,20 @@
 'use strict';
 
-window.appConfig = {
-  apiBasePath: window.apiBasePath,
-  orgLogo: '/images/logo.png',
-  orgColor: '#0a498a',
-  version: '2.0.0'
-};
 
-var CommonConstants = angular.module('broker.common.constants', [])
+angular.module('broker.common.constants', [])
   .constant('USER_ROLES', {
     all: '*',
     user: 'user',
     admin: 'admin'
   })
-  .constant('APP_CONFIG', angular.copy(window.appConfig))
+  .constant('APP_CONFIG', {
+    apiBasePath: angular.element('meta[name="api-base-path"]').attr('content'),
+    orgLogo: '/images/logo.png',
+    orgColor: '#0a498a',
+    version: '2.0.0'
+  })
   .constant('ROUTES', {
     login: '/login',
     logout: '/logout',
     default: '/login'
   });
-
-window.CommonConstants = CommonConstants
