81de810f732f6742e08c49fbc559eb7fc82a0c3c
AllenBW <wight_allen@bah.com>
2015-05-26 08:52:29 -0400

    updated api routes

--- src/client/app/components/api/api.constants.js
+++ src/client/app/components/api/api.constants.js
@@ -1,46 +1,50 @@
-(function() {
+(function () {
   'use strict';
 
   angular.module('app.components')
     .constant('apiRoutes', {
       'basePath': '/api',
       'routes': {
+        'settingById': '/settings/:id',
+        'settingsByHid': '/settings/:hid',
+        'cloudsById': '/clouds/:id',
 
-        'settingById':       '/settings/:id',
-        'settingsByHid':     '/settings/:hid',
-        'cloudsById':        '/clouds/:id',
+        'alerts': '/alerts',
+        'alertsById': '/alerts/:id',
 
-        'alerts':            '/alerts',
-        'alertsById':        '/alerts/:id',
+        'orders': '/orders/:id',
+        'orderItems': '/order_items/:id',
 
-        'orders':            '/orders/:id',
-        'orderItems':        '/order_items/:id',
+        'projectGroups': '/projects/:projectId/groups',
+        'projectGroupById': '/projects/:projectId/groups/:groupId',
+        'projectRoles': '/projects/:projectId/roles',
+        'projectRoleById': '/projects/:projectId/roles/:roleId',
+        'projectsById': '/projects/:id',
+        'projectQuestions': '/project_questions/:id',
+        'servicesById': '/services/:id',
 
-        'projectGroups':     '/projects/:projectId/groups',
-        'projectGroupById':  '/projects/:projectId/groups/:groupId',
-        'projectsById':      '/projects/:id',
-        'projectQuestions':  '/project_questions/:id',
-        'servicesById':      '/services/:id',
+        'productsById': '/products/:id',
+        'productTypesById': '/product_types/:id',
 
-        'productsById':      '/products/:id',
-        'productTypesById':  '/product_types/:id',
+        'signIn': '/staff/sign_in',
+        'signOut': '/staff/sign_out',
+        'ssoInit': '/saml/init',
 
-        'signIn':            '/staff/sign_in.json',
-        'signOut':           '/staff/sign_out.json',
-        'ssoInit':           '/saml/init',
+        'staffOrders': '/staff/:staff_id/orders/:id',
+        'staffById': '/staff/:id',
+        'staffSearch': '/staff?query=',
+        'currentMember': '/staff/current_member',
 
-        'staffOrders':       '/staff/:staff_id/orders/:id',
-        'staffById':         '/staff/:id',
-        'staffSearch':       '/staff?query=',
-        'currentMember':     '/staff/current_member',
+        'services': '/services',
+        'serviceAll': '/services/all_count',
+        'serviceProject': '/services/project_count',
+        'serviceOrderProfiles': '/services/order_profiles',
 
-        'services':           '/services',
-        'serviceAll':         '/services/all_count',
-        'serviceProject':     '/services/project_count',
-        'serviceOrderProfiles':     '/services/order_profiles',
-
-        'groups':            '/groups',
-        'groupsById':        '/groups/:id'
+        'groups': '/groups',
+        'groupsById': '/groups/:id',
+        'roles': '/roles',
+        'rolesById': '/roles/:id',
+        'wizardQuestions': '/wizard_questions/:id'
       }
     });
 })();
