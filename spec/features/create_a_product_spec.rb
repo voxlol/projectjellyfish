a02db7eb7e1145392a48b5adb701472ff76274ee
mafernando <fernando_michael@bah.com>
2015-05-27 15:12:15 -0500

    update feature spec use dummy storage object instead of vmware vm in create a product spec removes vmware from questions.json so it doesn't appear in marketplace

--- spec/features/create_a_product_spec.rb
+++ spec/features/create_a_product_spec.rb
@@ -5,17 +5,16 @@ feature 'Product creation' do
     staff = create(:staff, :admin)
     new_product_attributes = {
       'name' => 'Product Jellyfish',
-      'description' => 'A product description',
-      'product_type' => 'VMware VM',
-      'img' => 'products/aws_ec2.png',
+      'description' => 'A storage object',
+      'product_type' => 'Storage',
+      'img' => 'products/database.png',
       'active' => true,
       'setup_price' => 10.0000,
       'monthly_price' => 10.0000,
       'hourly_price' => 10.0000,
       'provisioning_answers' => {
-        'ram_size' => 2,
-        'disk_size' => 50,
-        'cpu_count' => '4'
+        'availability' => 'normal',
+        'region' => 'us-standard'
       }
     }
 
@@ -25,15 +24,18 @@ feature 'Product creation' do
 
     fill_in 'Name', with: new_product_attributes['name']
     fill_in 'Description', with: new_product_attributes['description']
+
     select(new_product_attributes['product_type'])
     selectize('#productImg', option: "[src$='#{new_product_attributes['img']}']")
+
     check('Active?')
+
     fill_in 'Setup Price', with: new_product_attributes['setup_price']
     fill_in 'Monthly Price', with: new_product_attributes['monthly_price']
     fill_in 'Hourly Price', with: new_product_attributes['hourly_price']
-    fill_in 'RAM size in GB', with: new_product_attributes['provisioning_answers']['ram_size']
-    fill_in 'Disk Size in GB', with: new_product_attributes['provisioning_answers']['disk_size']
-    select(new_product_attributes['provisioning_answers']['cpu_count'])
+    select(new_product_attributes['provisioning_answers']['availability'])
+    select(new_product_attributes['provisioning_answers']['region'])
+
     click_on 'CREATE PRODUCT'
 
     expect(page).to have_content('Product was successfully added.')
