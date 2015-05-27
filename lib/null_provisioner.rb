9aa20251ffcc0d4b4dade09d452f0f9bed1c0fb2
mafernando <fernando_michael@bah.com>
2015-05-27 11:21:55 -0500

    fix rubocop grumbles

--- lib/null_provisioner.rb
+++ lib/null_provisioner.rb
@@ -14,7 +14,7 @@ class NullProvisioner
   def provision
     @order_item = OrderItem.where(id: @order_item_id).first
     @order_item.provision_status = 'ok'
-    @order_item.payload_response = { defaults: {ip_address: '127.0.0.1', total: '0.0', hostname: 'TBD'} }
+    @order_item.payload_response = { defaults: { ip_address: '127.0.0.1', total: '0.0', hostname: 'TBD' } }
     @order_item.save!
     true
   end
