cf80a54373426f4059c580a4c4e289135d3cb67a
Michael Stack <michael.stack@gmail.com>
2015-06-12 20:10:53 -0500

    Went down a rabbit hole. Leaving this minor change after backing out of my other changes.
    - Orders, OrderItems, and now Memberships all need to be under the microscope.

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,3 +1,10 @@
 class ServiceSerializer < ApplicationSerializer
-  attributes :id, :project_id, :product_id, :provision_status, :latest_alert_id, :project_name, :project_description, :service_name, :service_description
+  attributes :id, :product_id, :provision_status, :latest_alert_id
+  attributes :service_name, :service_description
+  attributes :project_id, :project_name, :project_description
+
+  has_one :order
+  has_one :product
+  has_one :project
+  has_one :latest_alert
 end
