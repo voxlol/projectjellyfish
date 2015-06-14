3c3fbc1ca6af53abf27cb67ee72285efd660d6ad
Michael Stack <michael.stack@gmail.com>
2015-06-14 11:45:28 -0500

    Add serializers to limit output

--- app/serializers/order_item_serializer.rb
+++ app/serializers/order_item_serializer.rb
@@ -1,3 +1,38 @@
+# == Schema Information
+#
+# Table name: order_items
+#
+#  id                      :integer          not null, primary key
+#  order_id                :integer
+#  cloud_id                :integer
+#  product_id              :integer
+#  service_id              :integer
+#  provision_status        :integer
+#  created_at              :datetime
+#  updated_at              :datetime
+#  deleted_at              :datetime
+#  project_id              :integer
+#  miq_id                  :integer
+#  uuid                    :uuid
+#  setup_price             :decimal(10, 4)   default(0.0)
+#  hourly_price            :decimal(10, 4)   default(0.0)
+#  monthly_price           :decimal(10, 4)   default(0.0)
+#  payload_request         :jsonb
+#  payload_acknowledgement :jsonb
+#  payload_response        :jsonb
+#  latest_alert_id         :integer
+#  status_msg              :string
+#
+# Indexes
+#
+#  index_order_items_on_cloud_id    (cloud_id)
+#  index_order_items_on_deleted_at  (deleted_at)
+#  index_order_items_on_miq_id      (miq_id)
+#  index_order_items_on_order_id    (order_id)
+#  index_order_items_on_product_id  (product_id)
+#  index_order_items_on_service_id  (service_id)
+#
+
 class OrderItemSerializer < ApplicationSerializer
   attributes :id, :product_id, :project_id, :service_id, :provision_status, :latest_alert_id,
              :uuid, :setup_price, :monthly_price, :hourly_price, :status_msg, :created_at, :updated_at
