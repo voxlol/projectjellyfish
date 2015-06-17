a712df82e29ca3c2885406c67df2217ee9b49451
mafernando <fernando_michael@bah.com>
2015-06-17 17:33:31 -0500

    more cleaning up of alerts factory

--- spec/factories/alert.rb
+++ spec/factories/alert.rb
@@ -6,56 +6,32 @@ FactoryGirl.define do
     end_date 'NULL'
 
     trait :warning do
-      status 'WARNING'
+      status 'warning'
       message 'This is a WARNING alert.'
     end
 
     trait :critical do
-      status 'CRITICAL'
+      status 'critical'
       message 'This is a CRITICAL alert.'
     end
 
     trait :unknown do
-      status 'UNKNOWN'
+      status 'unknown'
       message 'This is an UNKNOWN alert.'
     end
 
     trait :active do
-      status 'WARNING'
+      status 'warning'
       message 'This alert is within its start and end dates.'
       start_date "#{Time.zone.now}" # START DATE NOW
       end_date "#{Time.zone.now + 1.day}" # END DATE NOT SET
     end
 
     trait :inactive do
-      status 'WARNING'
+      status 'warning'
       message 'This alert is not within its start and end dates.'
       start_date "#{Time.zone.now - 2.days}" # YESTERDAY - 1
       end_date "#{Time.zone.now - 1.day}" # YESTERDAY
     end
-
-    # trait :first do
-    #   project_id '3'
-    #   staff_id '0'
-    #   order_item_id '1'
-    #   status 'WARNING'
-    #   message 'This is a WARNING alert for the first service of project 3.'
-    # end
-    #
-    # trait :second do
-    #   project_id '3'
-    #   staff_id '0'
-    #   order_item_id '2'
-    #   status 'CRITICAL'
-    #   message 'This is a CRITICAL alert for the second service of project 3'
-    # end
-    #
-    # trait :third do
-    #   project_id '3'
-    #   staff_id '0'
-    #   order_item_id '3'
-    #   status 'UNKNOWN'
-    #   message 'This is an UNKNOWN alert for the third service of project 3'
-    # end
   end
 end
