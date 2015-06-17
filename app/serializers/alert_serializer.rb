7950baafe74b2d9c2c6b57340218c2dfb2ac3e66
mafernando <fernando_michael@bah.com>
2015-06-17 15:50:55 -0500

    removed deprecated alerts attributes

--- app/serializers/alert_serializer.rb
+++ app/serializers/alert_serializer.rb
@@ -21,6 +21,6 @@
 #
 
 class AlertSerializer < ApplicationSerializer
-  attributes :id, :project_id, :staff_id, :order_item_id, :status, :message
+  attributes :id, :status, :message
   attributes :start_date, :end_date, :created_at, :updated_at
 end
