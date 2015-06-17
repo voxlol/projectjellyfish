6f82ce495a360f5bf403d1857d2e4b8165c18576
mafernando <fernando_michael@bah.com>
2015-06-17 12:20:48 -0500

    commenting out apipie requirements because it makes me feel better

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -12,21 +12,21 @@ class AlertsController < ApplicationController
   before_action :load_alert_id, only: [:create, :sensu]
 
   api :GET, '/alerts', 'Returns all alerts.'
-  param :active, :bool, required: false
-  param :latest, :bool, required: false
-  param :sort, Array, required: false
-  param :not_status, Array, required: false
-  param :includes, Array, required: false, in: %w(project)
-  param :page, :number, required: false
-  param :per_page, :number, required: false
+  # param :active, :bool, required: false
+  # param :latest, :bool, required: false
+  # param :sort, Array, required: false
+  # param :not_status, Array, required: false
+  # param :includes, Array, required: false, in: %w(project)
+  # param :page, :number, required: false
+  # param :per_page, :number, required: false
 
   def index
     respond_with_params @alerts
   end
 
   api :GET, '/alerts/:id', 'Shows alert with :id'
-  param :id, :number, required: true
-  error code: 404, desc: MissingRecordDetection::Messages.not_found
+  # param :id, :number, required: true
+  # error code: 404, desc: MissingRecordDetection::Messages.not_found
 
   def show
     authorize @alert
@@ -34,15 +34,15 @@ class AlertsController < ApplicationController
   end
 
   api :POST, '/alerts/sensu', 'Create new Sensu alert.'
-  param :host, String, required: true, desc: 'The host of the product associated with this alert.'
-  param :port, String, required: true, desc: 'The port of the product associated with this alert.'
-  param :service, String, required: true, desc: 'Name of service deployed on host'
-  param :status, String, required: true, desc: 'Status message associated issued with this service from Sensu. <br>Current Options: OK, WARNING, CRITICAL, UNKNOWN, PENDING'
-  param :message, String, required: true, desc: 'Actual message content of alert.'
-  param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
-  param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
-  description 'Endpoint to push an alert from Sensu to API. <br>If an alert already exists for this service, then update that service.'
-  error code: 422, desc: ParameterValidation::Messages.missing
+  # param :host, String, required: true, desc: 'The host of the product associated with this alert.'
+  # param :port, String, required: true, desc: 'The port of the product associated with this alert.'
+  # param :service, String, required: true, desc: 'Name of service deployed on host'
+  # param :status, String, required: true, desc: 'Status message associated issued with this service from Sensu. <br>Current Options: OK, WARNING, CRITICAL, UNKNOWN, PENDING'
+  # param :message, String, required: true, desc: 'Actual message content of alert.'
+  # param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
+  # param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
+  # description 'Endpoint to push an alert from Sensu to API. <br>If an alert already exists for this service, then update that service.'
+  # error code: 422, desc: ParameterValidation::Messages.missing
 
   def sensu
     @alert = Alert.new @alert_params
@@ -58,15 +58,15 @@ class AlertsController < ApplicationController
   end
 
   api :POST, '/alerts', 'Creates a new alert'
-  param :project_id, String, required: true, desc: 'The project id issued with this new alert. <br>A 0 indicates system wide alert.'
-  param :staff_id, String, required: true, desc: 'The staff id of the user who is posting this new alert. <br>0 indicates a system generated alert.'
-  param :order_item_id, String, required: true, desc: 'The order item id associated with this new alert. <br>A 0 indicates a project wide alert.'
-  param :status, String, required: true, desc: 'HTTP status code issued with this alert. <br>Valid Options: OK, WARNING, CRITICAL, UNKNOWN, PENDING'
-  param :message, String, required: true, desc: 'The message content of the new alert.'
-  param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
-  param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
-  description 'Endpoint to create a new status alert for an order. <br>If a status alert is the same as the last one recorded for the service, then the existing status alert is updated. <br> Otherwise, a new status alert is created for the service.'
-  error code: 422, desc: ParameterValidation::Messages.missing
+  # param :project_id, String, required: true, desc: 'The project id issued with this new alert. <br>A 0 indicates system wide alert.'
+  # param :staff_id, String, required: true, desc: 'The staff id of the user who is posting this new alert. <br>0 indicates a system generated alert.'
+  # param :order_item_id, String, required: true, desc: 'The order item id associated with this new alert. <br>A 0 indicates a project wide alert.'
+  # param :status, String, required: true, desc: 'HTTP status code issued with this alert. <br>Valid Options: OK, WARNING, CRITICAL, UNKNOWN, PENDING'
+  # param :message, String, required: true, desc: 'The message content of the new alert.'
+  # param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
+  # param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
+  # description 'Endpoint to create a new status alert for an order. <br>If a status alert is the same as the last one recorded for the service, then the existing status alert is updated. <br> Otherwise, a new status alert is created for the service.'
+  # error code: 422, desc: ParameterValidation::Messages.missing
 
   def create
     @alert = Alert.new @alert_params
@@ -86,12 +86,12 @@ class AlertsController < ApplicationController
   end
 
   api :PUT, '/alerts/:id', 'Updates alert with given :id'
-  param :staff_id, String, required: false, desc: 'The staff id of the user who is posting this new alert. <br>0 indicates a system generated alert.'
-  param :message, String, required: false, desc: 'The message content to update alert with.'
-  param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
-  param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
-  description 'Endpoint to update an existing alert. <br>To preserve referential integrity, only the following attributes can be changed: message, start_date, end_date.'
-  error code: 404, desc: MissingRecordDetection::Messages.not_found
+  # param :staff_id, String, required: false, desc: 'The staff id of the user who is posting this new alert. <br>0 indicates a system generated alert.'
+  # param :message, String, required: false, desc: 'The message content to update alert with.'
+  # param :start_date, String, required: false, desc: 'Date this alert will begin appearing. Null indicates the alert will start appearing immediately.'
+  # param :end_date, String, required: false, desc: 'Date this alert should no longer be displayed after. Null indicates the alert does not expire.'
+  # description 'Endpoint to update an existing alert. <br>To preserve referential integrity, only the following attributes can be changed: message, start_date, end_date.'
+  # error code: 404, desc: MissingRecordDetection::Messages.not_found
 
   def update
     authorize @alert
@@ -100,8 +100,8 @@ class AlertsController < ApplicationController
   end
 
   api :DELETE, '/alerts/:id', 'Deletes alert with :id'
-  param :id, :number, required: true
-  error code: 404, desc: MissingRecordDetection::Messages.not_found
+  # param :id, :number, required: true
+  # error code: 404, desc: MissingRecordDetection::Messages.not_found
 
   def destroy
     authorize @alert
