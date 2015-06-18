65970b4651f01900420026011e65ce803f176f9d
mafernando <fernando_michael@bah.com>
2015-06-18 09:55:03 -0500

    make category a permitted param on alerts controller

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -65,6 +65,6 @@ class AlertsController < ApplicationController
   end
 
   def alert_params
-    @_alert_params ||= params.permit(:status, :message, :start_date, :end_date)
+    @_alert_params ||= params.permit(:status, :message, :category, :start_date, :end_date)
   end
 end
