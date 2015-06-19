79989cdf71502a536fe6eb4062c27005fdbd030d
mafernando <fernando_michael@bah.com>
2015-06-19 11:51:47 -0500

    added latest service alerts as a method that can be called on a project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -66,7 +66,7 @@ class Project < ActiveRecord::Base
   end
 
   def compute_current_status!
-    if latest_alerts.any?
+    if latest_service_alerts.any?
       update(status: highest_priority_latest_alert.status.downcase)
     else
       update(status: 'unknown')
@@ -94,7 +94,7 @@ class Project < ActiveRecord::Base
   end
 
   def problem_count
-    @problem_count ||= latest_alerts.count { |a| a unless a.status == 'ok' }
+    @problem_count ||= latest_service_alerts.count { |a| a unless a.status == 'ok' }
   end
 
   def account_number
@@ -121,13 +121,11 @@ class Project < ActiveRecord::Base
     '2 GB'
   end
 
-  # private
-
-  def latest_alerts
+  def latest_service_alerts
     services.map { |s| s.alerts.where(id: s.alerts.group('category').select('max(id) as latest_alert_id')) }.flatten
   end
 
   def highest_priority_latest_alert
-    latest_alerts.max_by { |alert| STATES[alert.status.downcase] }
+    latest_service_alerts.max_by { |alert| STATES[alert.status.downcase] }
   end
 end
