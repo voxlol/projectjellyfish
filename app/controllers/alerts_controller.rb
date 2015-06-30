19422881aca3af88f95e0c26932cd857fd52af04
mafernando <fernando_michael@bah.com>
2015-06-30 16:36:25 -0500

    add id as an alert_param in alerts controller

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -99,6 +99,6 @@ class AlertsController < ApplicationController
   end
 
   def alert_params
-    @_alert_params ||= params.permit(:status, :message, :category, :start_date, :end_date, :alertable_type, :alertable_id)
+    @_alert_params ||= params.permit(:id, :status, :message, :category, :start_date, :end_date, :alertable_type, :alertable_id)
   end
 end
