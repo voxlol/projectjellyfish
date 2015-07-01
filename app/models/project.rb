d389b4f09fd79ea77b1de634c5a17c39c4640606
Jerimiah Milton <milton_jerimiah@bah.com>
2015-06-30 21:36:23 -0500

    Added group members to groups object.

--- app/models/project.rb
+++ app/models/project.rb
@@ -38,6 +38,7 @@ class Project < ActiveRecord::Base
   has_many :memberships
   has_many :groups, through: :memberships
   has_many :staff, through: :groups
+  has_many :groups_staffs, through: :groups, source: :staff
   has_many :services, foreign_key: 'project_id', class_name: 'OrderItem'
   has_many :alerts
   has_many :latest_alerts, through: :services, class_name: 'Alert'
