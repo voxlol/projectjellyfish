7fde7f1e652aacf1efc7cc4e8c58ccec44a0bb8f
AllenBW <wight_allen@bah.com>
2015-07-02 00:16:46 -0400

    motd resource created

---
+++ src/client/app/resources/motd.factory.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular.module('app.resources')
+    .factory('Motd', MotdFactory);
+
+  /** @ngInject */
+  function MotdFactory($resource) {
+    var Motd = $resource('/api/v1/motd/:id' , {id: '@id'}, {
+      
+      'update': {
+        method: 'PUT',
+        isArray: false
+      }
+    });
+
+    return Motd;
+  }
+})();
