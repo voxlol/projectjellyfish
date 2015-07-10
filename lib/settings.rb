9f9f8518d280288687050dd9d5f0d70c1cdfd87b
Michael Stack <michael.stack@gmail.com>
2015-07-10 14:50:48 -0500

    Update SETTINGS load file

--- lib/settings.rb
+++ lib/settings.rb
@@ -6,13 +6,11 @@ settings_file = ["settings.#{Rails.env}.yml", 'settings.yml'].find do |file|
 end
 
 if settings_file
-  SETTINGS = YAML.load_file("#{root}/#{settings_file}")
+  SETTINGS = YAML.load_file(File.join Rails.root, 'config', settings_file)
   SETTINGS[:version] = Jellyfish::Version.new
-  SETTINGS[:unattended] = SETTINGS[:unattended].nil? || SETTINGS[:unattended]
-  SETTINGS[:login] ||= SETTINGS[:ldap]
 
-# Load plugin config, if any
-  Dir["#{root}/config/settings.plugins.d/*.yml"].each do |f|
+  # Load plugin config, if any
+  Dir[File.join Rails.root, 'config', 'settings.d', '*.yml'].each do |f|
     SETTINGS.merge! YAML.load_file f
   end
 else
