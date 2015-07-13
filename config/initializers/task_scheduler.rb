15b5ace97b6fe2388b413e97ed8d7e05e458ad44
mafernando <fernando_michael@bah.com>
2015-07-13 12:17:12 -0500

    prepending bundle exec to accommodate grumpy installs.

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -8,9 +8,9 @@ require 'rake'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '24h' do
-  system('rake upkeep:prune_alerts')
+  system('bundle exec rake upkeep:prune_alerts')
 end
 
 scheduler.every '15m' do
-  system('rake upkeep:update_budgets')
+  system('bundle exec rake upkeep:update_budgets')
 end
