86b8898735a3899003e410e48e58276eb12aa6b9
Michael Stack <michael.stack@gmail.com>
2015-07-25 02:16:42 -0500

    Refactor products-table
    - Remove redefinition of table classes
    - Fetch ProductType from models
    - Remove redundant styles

--- src/client/app/components/products-table/products-table.directive.js
+++ src/client/app/components/products-table/products-table.directive.js
@@ -9,7 +9,8 @@
     var directive = {
       restrict: 'AE',
       scope: {
-        products: '='
+        products: '=',
+        productTypes: '='
       },
       link: link,
       templateUrl: 'app/components/products-table/products-table.html',
@@ -25,12 +26,24 @@
     }
 
     /** @ngInject */
-    function ProductsTableController() {
+    function ProductsTableController(lodash) {
       var vm = this;
 
       vm.activate = activate;
 
       function activate() {
+        setProductTypes();
+      }
+
+      // Private
+
+      function setProductTypes() {
+        angular.forEach(vm.products, setProductType);
+
+        function setProductType(product) {
+          var productType = lodash.find(vm.productTypes, {id: product.product_type_id});
+          product.product_type = productType ? productType.name : 'Unknown';
+        }
       }
     }
   }
