14a45a0a18c1c8ed0b535d59ad4c7bfbf53bfa95
Michael Stack <michael.stack@gmail.com>
2015-08-31 13:34:00 -0500

    Add ProjectService directive
    Used to list services belonging to a project

---
+++ src/client/app/components/project-services/project-services.directive.js
@@ -0,0 +1,38 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .directive('projectServices', ProjectServicesDirective);
+
+  /** @ngInject */
+  function ProjectServicesDirective() {
+    var directive = {
+      restrict: 'AE',
+      scope: {
+        projectId: '=',
+        services: '='
+      },
+      link: link,
+      templateUrl: 'app/components/project-services/project-services.html',
+      controller: ProjectServicesController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    function link(scope, element, attrs, vm, transclude) {
+      vm.activate();
+    }
+
+    /** @ngInject */
+    function ProjectServicesController() {
+      var vm = this;
+
+      vm.activate = activate;
+
+      function activate() {
+      }
+    }
+  }
+})();
