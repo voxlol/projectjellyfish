0afe725c641f7c857a93bdf1a1109ba3c5d125b9
Michael Stack <michael.stack@gmail.com>
2015-09-11 16:18:33 -0500

    ValueTypes must be a module

--- app/models/concerns/value_types.rb
+++ app/models/concerns/value_types.rb
@@ -1,23 +1,25 @@
-class ValueTypes
+module ValueTypes
   extend ActiveSupport::Concern
 
-  validates :value, uri: true, if: -> (s) { s.value_type == 'url' }
-  validates :value, email: true, if: -> (s) { s.value_type == 'email' }
-  # TODO: Add more type validations
+  included do
+    validates :value, uri: true, if: -> (s) { s.value_type == 'url' }
+    validates :value, email: true, if: -> (s) { s.value_type == 'email' }
+    # TODO: Add more type validations
 
-  enum value_type: {
-    string: 0,
-    password: 1,
-    integer: 2,
-    boolean: 3,
-    array: 4,
-    json: 5,
-    date: 6,
-    datetime: 7,
-    fingerprint: 8,
-    certificate: 9,
-    text: 10,
-    url: 11,
-    email: 12
-  }
+    enum value_type: {
+      string: 0,
+      password: 1,
+      integer: 2,
+      boolean: 3,
+      array: 4,
+      json: 5,
+      date: 6,
+      datetime: 7,
+      fingerprint: 8,
+      certificate: 9,
+      text: 10,
+      url: 11,
+      email: 12
+    }
+  end
 end
