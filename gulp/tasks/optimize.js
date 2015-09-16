4b05797ab9466879a955abf6301908dc8461a1f0
Michael Stack <michael.stack@gmail.com>
2015-09-16 16:02:07 -0500

    Disable minification and annotations for application code
    - Vendor libraries are still minified
    - Update some outdated libraries

--- gulp/tasks/optimize.js
+++ gulp/tasks/optimize.js
@@ -39,8 +39,12 @@ module.exports = function(gulp, options) {
       .pipe(cssFilter.restore)
       // Get the custom javascript
       .pipe(jsAppFilter)
-      .pipe(ngAnnotate(config.ngAnnotateOptions))
-      .pipe(uglify())
+
+      // FIXME Disabling minification and annoations until this issue is fixed
+      // https://github.com/olov/ng-annotate/issues/168
+      //.pipe(ngAnnotate(config.ngAnnotateOptions))
+      //.pipe(uglify())
+
       .pipe(getHeader())
       .pipe(jsAppFilter.restore)
       // Get the vendor javascript
