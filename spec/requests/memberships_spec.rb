8c5a7d5b20b854f1426de36c630c68f576ff4d37
mafernando <fernando_michael@bah.com>
2015-09-21 10:01:32 -0500

    Implement index spec for memberships on project

--- spec/requests/memberships_spec.rb
+++ spec/requests/memberships_spec.rb
@@ -4,7 +4,27 @@ RSpec.describe 'Memberships API' do
   let(:default_params) { { format: :json } }
 
   describe 'GET index' do
-    it 'lists all available on a project'
+    it 'lists all available on a project' do
+      sign_in_as create :staff, :admin
+      # CREATE TWO GROUPS
+      group1 = create(:group)
+      group2 = create(:group)
+
+      # CREATE TWO ROLES
+      role1 = create(:role)
+      role2 = create(:role)
+
+      # CREATE A PROJECT
+      project = create(:project)
+
+      # ASSOCIATE GROUPS WITH ROLES AGAINST PROJECT
+      post project_memberships_path(project, group_id: group1.id, role_id: role1.id)
+      post project_memberships_path(project, group_id: group2.id, role_id: role2.id)
+
+      project.reload
+
+      expect(project.memberships.count).to eq(2)
+    end
   end
 
   describe 'POST create' do
