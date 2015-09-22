ae799d48fd1fd7bfdfb7b5c698baf219ce772e20
Sonja Hall <hall_sonja@bah.com>
2015-09-22 15:20:45 -0400

    added in the dynamic version to the footer via its directive and html

--- src/client/app/components/footer/footer.directive.js
+++ src/client/app/components/footer/footer.directive.js
@@ -8,9 +8,42 @@
   function FooterDirective() {
     var directive = {
       restrict: 'AE',
-      templateUrl: 'app/components/footer/footer.html'
+      scope: {
+        jellyfish_version: '='
+      },
+      replace: true,
+      link: link,
+      templateUrl: 'app/components/footer/footer.html',
+      controller: SulyController,
+      controllerAs: 'vm',
+      bindToController: true
     };
 
     return directive;
+
+    function link(scope, element, attrs, vm, transclude, Version) {
+      vm.activate();
+    }
+
+    /** @ngInject */
+    function SulyController($http) {
+      var vm = this;
+
+      vm.activate = activate;
+
+      function activate() {
+        initVersion();
+      }
+
+      function initVersion() {
+        $http.get('/api/v1/version/')
+          .then(handleResults);
+
+        function handleResults(results) {
+          vm.version = results.data.jellyfish_version;
+          console.log(results.data.jellyfish_version);
+        }
+      }
+    }
   }
 })();
