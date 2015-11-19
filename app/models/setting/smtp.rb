24fa3cdc4eefb7e29fc74222266e3c192efaf05b
mafernando <fernando_michael@bah.com>
2015-11-19 10:28:10 -0600

    Change SMTP user name setting to string type
    This was originally password type because Postmark uses your API Token
    as user name and password. Didn't want API Token showing as user name
    in plain text under settings.

--- app/models/setting/smtp.rb
+++ app/models/setting/smtp.rb
@@ -7,7 +7,7 @@ class Setting
           set('smtp_enabled', description: 'SMTP Enabled', value_type: :boolean, default: false),
           set('smtp_address', description: 'SMTP Server IP Address', value_type: :string, default: 'address'),
           set('smtp_port', description: 'SMTP Server Port', value_type: :string, default: 'port'),
-          set('smtp_user_name', description: 'SMTP User Name', value_type: :password, default: 'user_name'),
+          set('smtp_user_name', description: 'SMTP User Name', value_type: :string, default: 'user_name'),
           set('smtp_password', description: 'SMTP Password', value_type: :password, default: 'password'),
           set('smtp_authentication', description: 'SMTP Authentication Method', value_type: :string, default: 'plain'),
           set('smtp_default_sender', description: 'SMTP Default Sender', value_type: :string, default: 'sender@domain.com')
