7f3f6a25bd670771e8877e0708a7e61176011f1b
mafernando <fernando_michael@bah.com>
2015-11-19 09:23:32 -0600

    Remove redundant initializer config
    Verified that action_mailer delivery method is set to smtp by default
    for dev and prod while test is set to its namesake.

--- config/initializers/listeners.rb
+++ config/initializers/listeners.rb
@@ -1,10 +1,5 @@
 require 'wisper'
 
-Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :test if Rails.env.test?
-end
-
 Rails.application.config.to_prepare do
   Wisper.clear if Rails.env.development?
   Wisper::GlobalListeners.subscribe(SMTPListener.new)
