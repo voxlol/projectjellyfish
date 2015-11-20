9196fe75a71e696b325604ee2c8e7daae0d2827e
mafernando <fernando_michael@bah.com>
2015-11-19 21:29:37 -0600

    Make dev default to putting mail in console
    Change development initializer to set action_mailer.delivery_method to
    :test so emails appear in console for review in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.action_mailer.delivery_method = :test
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
